534bd4e9382e5a80d1ce1bb2424e599f6bffc002
Oscar Beaumont <oscar@otbeaumont.me>
2022-11-01 23:22:50 +0800

    devtools in prod

--- utils/client.ts
+++ utils/client.ts
@@ -5,4 +5,4 @@ import { QueryClient } from "@tanstack/query-core";
 export const queryClient = new QueryClient();
 
 export { Hydrate, QueryClientProvider } from "@tanstack/react-query";
-export { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+export { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/index.prod.js"; // This weird import is to allow it to work in prod builds
